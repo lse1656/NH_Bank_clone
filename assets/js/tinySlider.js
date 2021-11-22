1fdd0d230966cb25bd45456de864f297f2ae5aff
lse1656 <lse1656@naver.com>
2021-11-22 15:38:13 +0900

    ♻️ tinySlider 파일 따로 분리

---
+++ assets/js/tinySlider.js
@@ -0,0 +1,12 @@
+var productCarouel = tns({
+  container: '.visual-carousel .visual-carousel-slider',
+  items: 1,
+  slideBy: 'page',
+  autoplay: true,
+  controls: false,
+  autoplayPosition: 'bottom',
+  navContainer: '.visual-carousel .visual-carousel-nav',
+  autoplayButton: '.visual-carousel .visual-carousel-autoplay',
+  autoplayButtonOutput: false,
+  autoplayText: ['', ''],
+});
